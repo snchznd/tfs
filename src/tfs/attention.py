09c4dada124f232d73746a05c22f5ab3aad5ab23
Miguel <miguel.ngl.sanchez@gmail.com>
2025-03-06 00:23:48 +0100

    Add causal mask implementation

--- src/tfs/attention.py
+++ src/tfs/attention.py
@@ -4,7 +4,7 @@ import torch
 
 
 class SelfAttention(torch.nn.Module):
-    def __init__(self, d: int, d_k: int, d_v: int) -> None:
+    def __init__(self, d: int, d_k: int, d_v: int, causal_mask: bool = True) -> None:
         super().__init__()
         self.W_q = torch.nn.Parameter(torch.zeros(size=(d, d_k)))
         self.W_k = torch.nn.Parameter(torch.zeros(size=(d, d_k)))
@@ -14,26 +14,45 @@ class SelfAttention(torch.nn.Module):
         torch.nn.init.xavier_normal_(self.W_v)
         self.softmax = torch.nn.Softmax(dim=2)
         self.normalization_factor = math.sqrt(d_k)
+        self.causal_mask = causal_mask
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
+        # compute matrices
         Q = X @ self.W_q
         K = X @ self.W_k
         V = X @ self.W_v
-        A = self.softmax(Q @ K.transpose(1, 2) / self.normalization_factor)
+
+        # compute un-normalized attention matrix
+        A = Q @ K.transpose(1, 2) / self.normalization_factor
+
+        # perform causal masking
+        if self.causal_mask:
+            seq_len = X.shape[1]
+            mask = torch.tril(torch.ones(size=(seq_len,) * 2)) == 0
+            A.masked_fill(mask=mask, value=-torch.inf)
+
+        # normalize attention scores
+        A = self.softmax(A)
+
         return A @ V
 
 
 class MultiHeadSelfAttention(torch.nn.Module):
-    def __init__(self, d: int, d_k: int, d_v: int, nbr_heads: int) -> None:
+    def __init__(
+        self, d: int, d_k: int, d_v: int, nbr_heads: int, causal_mask: bool = True
+    ) -> None:
         if d_v % nbr_heads != 0:
             raise ValueError(
                 "The output dim needs to be a multiple of the number of attention heads."
             )
         super().__init__()
         self.attention_heads = torch.nn.ModuleList(
-            [SelfAttention(d, d_k, d_v // nbr_heads) for _ in range(nbr_heads)]
+            [
+                SelfAttention(d, d_k, d_v // nbr_heads, causal_mask)
+                for _ in range(nbr_heads)
+            ]
         )
-        self.H = torch.nn.Parameter(torch.zeros(size=(d_v, d_v)))
+        self.H = torch.nn.Parameter(torch.zeros(size=(d_v, d)))
         torch.nn.init.xavier_normal_(self.H)
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
@@ -41,4 +60,4 @@ class MultiHeadSelfAttention(torch.nn.Module):
 
 
 # TODO:
-# - add causal attention masks
+# add biases in attention layer
