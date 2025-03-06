eefa4efa23ec2051d11b508045ae3d49650d8c8b
Miguel <miguel.ngl.sanchez@gmail.com>
2025-03-06 19:50:33 +0100

    Add biases to attention layer

--- src/tfs/attention.py
+++ src/tfs/attention.py
@@ -6,21 +6,32 @@ import torch
 class SelfAttention(torch.nn.Module):
     def __init__(self, d: int, d_k: int, d_v: int, causal_mask: bool = True) -> None:
         super().__init__()
+
+        # create matrices
         self.W_q = torch.nn.Parameter(torch.zeros(size=(d, d_k)))
         self.W_k = torch.nn.Parameter(torch.zeros(size=(d, d_k)))
         self.W_v = torch.nn.Parameter(torch.zeros(size=(d, d_v)))
+
+        # initialize matrices
         torch.nn.init.xavier_normal_(self.W_q)
         torch.nn.init.xavier_normal_(self.W_k)
         torch.nn.init.xavier_normal_(self.W_v)
+
+        # create biases
+        self.b_q = torch.nn.Parameter(torch.zeros(size=(d_k,)))
+        self.b_k = torch.nn.Parameter(torch.zeros(size=(d_k,)))
+        self.b_v = torch.nn.Parameter(torch.zeros(size=(d_v,)))
+
+        # create other elements
         self.softmax = torch.nn.Softmax(dim=2)
         self.normalization_factor = math.sqrt(d_k)
         self.causal_mask = causal_mask
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         # compute matrices
-        Q = X @ self.W_q
-        K = X @ self.W_k
-        V = X @ self.W_v
+        Q = X @ self.W_q + self.b_q
+        K = X @ self.W_k + self.b_k
+        V = X @ self.W_v + self.b_v
 
         # compute un-normalized attention matrix
         A = Q @ K.transpose(1, 2) / self.normalization_factor
@@ -57,7 +68,3 @@ class MultiHeadSelfAttention(torch.nn.Module):
 
     def forward(self, X: torch.Tensor) -> torch.Tensor:
         return torch.concat([A(X) for A in self.attention_heads], dim=2) @ self.H
-
-
-# TODO:
-# add biases in attention layer
