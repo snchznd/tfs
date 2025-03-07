c21c1aaaea71b13d40405cd9130f7b0c039bf8b1
Miguel <miguel.ngl.sanchez@gmail.com>
2025-03-07 21:34:33 +0100

    Enhance MLP in transformer layer

--- src/tfs/layer/transformer_layer.py
+++ src/tfs/layer/transformer_layer.py
@@ -5,11 +5,25 @@ from tfs.attention.multi_head_self_attention import MultiHeadSelfAttention
 
 class TransformerLayer(torch.nn.Module):
     def __init__(
-        self, d: int, d_k: int, d_h: int, nbr_heads: int, causal_mask: bool = True
+        self,
+        d: int,
+        d_k: int,
+        d_h: int,
+        nbr_heads: int,
+        causal_mask: bool = True,
+        ffn_factor: int = 4,
     ):
         super().__init__()
-        self.multi_head_self_attention = MultiHeadSelfAttention(d, d_k, d_h, nbr_heads)
-        self.mlp = torch.nn.Linear(in_features=d, out_features=d)
+        self.multi_head_self_attention = MultiHeadSelfAttention(
+            d, d_k, d_h, nbr_heads, causal_mask
+        )
+        self.mlp = torch.nn.Sequential(
+            torch.nn.Linear(in_features=d, out_features=d * ffn_factor),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=d * ffn_factor, out_features=d),
+        )
+
+        # torch.nn.Linear(in_features=d, out_features=d)
         self.layer_norm_0 = torch.nn.LayerNorm(normalized_shape=d)
         self.layer_norm_1 = torch.nn.LayerNorm(normalized_shape=d)
 
