4943a3788b901fab6645f59881b4c811d7b53a7b
Miguel <miguel.ngl.sanchez@gmail.com>
2025-03-07 20:00:39 +0100

    Add bias to multi-head self attention and refactor

--- src/tfs/multi_head_self_attention.py
+++ src/tfs/multi_head_self_attention.py
@@ -5,21 +5,31 @@ from tfs.self_attention import SelfAttention
 
 class MultiHeadSelfAttention(torch.nn.Module):
     def __init__(
-        self, d: int, d_k: int, d_v: int, nbr_heads: int, causal_mask: bool = True
+        self, d: int, d_k: int, d_h: int, nbr_heads: int, causal_mask: bool = True
     ) -> None:
-        if d_v % nbr_heads != 0:
+        if d_h % nbr_heads != 0:
             raise ValueError(
                 "The output dim needs to be a multiple of the number of attention heads."
             )
         super().__init__()
         self.attention_heads = torch.nn.ModuleList(
             [
-                SelfAttention(d, d_k, d_v // nbr_heads, causal_mask)
+                SelfAttention(d, d_k, d_h // nbr_heads, causal_mask)
                 for _ in range(nbr_heads)
             ]
         )
-        self.H = torch.nn.Parameter(torch.zeros(size=(d_v, d)))
+
+        # create output projection matrix
+        self.H = torch.nn.Parameter(torch.zeros(size=(d_h, d)))
+
+        # initialize output projection matrix
         torch.nn.init.xavier_normal_(self.H)
 
+        # create output projection bias
+        self.b_h = torch.nn.Parameter(torch.zeros(size=(d,)))
+
     def forward(self, X: torch.Tensor) -> torch.Tensor:
-        return torch.concat([A(X) for A in self.attention_heads], dim=2) @ self.H
+        return (
+            torch.concat([H(X) for H in self.attention_heads], dim=2) @ self.H
+            + self.b_h
+        )
